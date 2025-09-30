07ac135d0e16a02cfeeb7154c3cfdab0d5b108ed
ninnis29 <moremonje88@gmail.com>
2025-09-30 18:12:34 -0300

    pantalla inicio yeiyei

---
+++ facturae/Scripts/pantalla_inicio.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	pass 
+
+
+func _on_config_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+	pass 
