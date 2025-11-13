184f1cb9c4972abcca5278b426fba0824c84ba85
ninnis29 <moremonje88@gmail.com>
2025-11-12 23:58:51 -0300

    escenas finales animadas y themes

--- facturae/Scripts/pantalla_victoria.gd
+++ facturae/Scripts/pantalla_victoria.gd
@@ -1,6 +1,8 @@
 extends Node2D
+@onready var animacion : AnimationPlayer = $AnimationEscenaVictoria
 
-
+func _ready() -> void:
+	animacion.play("Dialogo")
 
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/pantalla_inicio.tscn")
